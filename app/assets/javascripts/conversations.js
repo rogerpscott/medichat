ba88bc593b2925daed5ff31806c2eb962a02946d
roger scott <rogerpscott@gmail.com>
2016-12-15 20:00:54 -0200

    notifications working design or message order

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -2,6 +2,16 @@ $(document).ready( function() {
   if ($('#kapor').text().length) {
     submitNewMessage();
   }
+
+  $('body').off('click','#btnSendMessage');
+  $('body').on("click","#btnSendMessage",function(event){
+    event.preventDefault();
+    if ($('#kapor').text().length) {
+      $('[data-send="message"]').click();
+      $('[data-textarea="message"]').val(" ")
+    }
+  });
+  
 });
 
 function submitNewMessage(){
